848224ef7c79827ab9e9e3c10f4647a08a2abee5
Hm-source <swo8852@gmail.com>
2024-12-17 09:50:17 +0900

    comment: write comments for code that needs to be modified.[restaurant1]

--- src/main/java/com/example/woowa/restaurant/category/dto/request/CategoryCreateRequest.java
+++ src/main/java/com/example/woowa/restaurant/category/dto/request/CategoryCreateRequest.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 public class CategoryCreateRequest {
 
     @Size(min = 1, max = 10, message = "카테고리명은 1자이상 10자이하여야 합니다.")
+    // @NotNull(message = "카테고리명은 null일 수 없습니다.")
     private final String name;
 
     @JsonCreator
--- src/main/java/com/example/woowa/restaurant/category/entity/Category.java
+++ src/main/java/com/example/woowa/restaurant/category/entity/Category.java
@@ -28,24 +28,27 @@ public class Category extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    // 논리 삭제 시, orphanRemoval = true 설정 삭제 필요.
+    // isDeleted 추가.
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<RestaurantCategory> restaurantCategories = new ArrayList<>();
 
     @Column(unique = true, nullable = false, length = 10)
     private String name;
 
+    // DTO 또는 서비스 레이어에서 유효성을 검사하도록 설계하되, 엔티티 레벨에서도 최소한의 데이터 무결성을 보장하도록.
     @Builder
     public Category(String name) {
         this.name = name;
     }
 
+    // restaurantCategories.add(restaurantCategory); restaurantCategories 리스트에 실제로 restaurantCategory가 추가 필요.
     public void addRestaurantCategory(RestaurantCategory restaurantCategory) {
         if (!Objects.equals(restaurantCategory.getCategory().getId(), this.getId())) {
             restaurantCategory.setCategory(this);
         }
     }
-
+    // 유효성 검사를 추가하여 잘못된 값이 설정되지 않도록 방지
     public void changeName(String name) {
         this.name = name;
     }
