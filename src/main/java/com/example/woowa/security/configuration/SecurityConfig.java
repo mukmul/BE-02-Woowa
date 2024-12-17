63706d74b7c6f0426f39b231a855354f1984dfbd
JinjinBread <pge0720@naver.com>
2024-12-17 10:42:19 +0900

    fix: change security configuration

--- src/main/java/com/example/woowa/security/configuration/SecurityConfig.java
+++ src/main/java/com/example/woowa/security/configuration/SecurityConfig.java
@@ -1,63 +1,67 @@
 package com.example.woowa.security.configuration;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-
 import com.example.woowa.security.filter.CustomerAuthenticationFilter;
 import com.example.woowa.security.filter.CustomerAuthorizationFilter;
 import com.example.woowa.security.user.UserRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        CustomerAuthenticationFilter customerAuthenticationFilter = new CustomerAuthenticationFilter(
-            authenticationManagerBean());
-
-        customerAuthenticationFilter.setFilterProcessesUrl("/baemin/v1/login");
-        http.
-            csrf().disable().
-            sessionManagement().sessionCreationPolicy(STATELESS).
-            and().
-            authorizeRequests().antMatchers(HttpMethod.GET, "/baemin/v1/login/**").permitAll().
-            and().
-            authorizeRequests().antMatchers(HttpMethod.POST, "/baemin/v1/owners").permitAll().
-            and().
-            authorizeRequests().antMatchers("/baemin/v1/owners/**").hasAnyAuthority(UserRole.ROLE_OWNER.toString()).
-            and().
-            authorizeRequests().anyRequest().authenticated().
-            and().
-            addFilter(customerAuthenticationFilter);
-
-        http.addFilterBefore(new CustomerAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    @Bean
+    public AuthenticationManager getAuthenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManager();
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+
+        CustomerAuthenticationFilter customerAuthenticationFilter = new CustomerAuthenticationFilter(authenticationManager);
+        customerAuthenticationFilter.setFilterProcessesUrl("/baemin/v1/login");
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                //회원가입, 개인정보 동의, 로그인, 로그아웃, 이메일 인증, 아이디 중복 확인, 아이디 및 비밀번호 찾기, 파비콘
+                                .requestMatchers("/baemin/v1/login/**", "/baemin/v1/owners").permitAll()
+                                .requestMatchers("/baemin/v1/owners/**")
+                                .hasAnyAuthority(UserRole.ROLE_OWNER.toString())
+                                .anyRequest().authenticated()
+                )
+                .addFilter(customerAuthenticationFilter)
+                .addFilterBefore(new CustomerAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
 }
