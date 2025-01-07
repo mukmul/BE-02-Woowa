5c46e3416667682bdfcd45a1617ab21d809f791c
hyunoi <nhe0622@gmail.com>
2025-01-08 00:26:33 +0900

    refactor: deleted log to MenuGroupApiController

--- src/main/java/com/example/woowa/restaurant/menugroup/controller/MenuGroupApiController.java
+++ src/main/java/com/example/woowa/restaurant/menugroup/controller/MenuGroupApiController.java
@@ -31,9 +31,7 @@ public class MenuGroupApiController {
     public ResponseEntity<Void> addMenuGroup(@PathVariable Long restaurantId,
             @RequestBody @Validated
             MenuGroupSaveRequest request) {
-        log.info("Request to create MenuGroup for Restaurant ID: {}, INFO: {}", restaurantId, request);
         Long menuGroupId = menuGroupService.addMenuGroup(restaurantId, request);
-        log.info("Create MenuGroup ID: {}", menuGroupId);
 
         // 메뉴 그룹 단건 조회 api
         return ResponseEntity.created(URI.create("/api/v1/menu-groups/" + menuGroupId))
@@ -43,9 +41,7 @@ public class MenuGroupApiController {
     // 메뉴 그룹 단건 조회
     @GetMapping("/api/v1/menu-groups/{menuGroupId}")
     public ResponseEntity<MenuGroupResponse> findMenuGroup(@PathVariable Long menuGroupId) {
-        log.info("Request to find MenuGroup ID: {}", menuGroupId);
         MenuGroupResponse response = menuGroupService.findMenuById(menuGroupId);
-        log.info("Successfully found MenuGroup Response: {}", response);
 
         return ResponseEntity.ok(response);
     }
@@ -54,9 +50,7 @@ public class MenuGroupApiController {
     @GetMapping("/api/v1/restaurant/{restaurantId}/menu-groups")
     public ResponseEntity<MenuGroupListResponse> findMenuGroupList(
             @PathVariable Long restaurantId) {
-        log.info("Request to find MenuGroup List for Restaurant ID: {}", restaurantId);
         MenuGroupListResponse response = menuGroupService.findMenuGroupByRestaurant(restaurantId);
-        log.info("Successfully found MenuGroup List Response: {}", response);
 
         return ResponseEntity.ok(response);
     }
@@ -66,18 +60,14 @@ public class MenuGroupApiController {
     public ResponseEntity<Void> updateMenuGroup(@PathVariable Long menuGroupId,
             @RequestBody @Validated
             MenuGroupUpdateRequest request) {
-        log.info("Request to update MenuGroup ID: {}, INFO: {}", menuGroupId, request);
         menuGroupService.updateMenuGroup(menuGroupId, request);
-        log.info("Successfully updated MenuGroup ID: {}", menuGroupId);
         return ResponseEntity.ok().build();
     }
 
     // 메뉴 그룹 삭제
     @DeleteMapping("/api/v1/menu-groups/{menuGroupId}")
     public ResponseEntity<Void> deleteMenuGroup(@PathVariable Long menuGroupId) {
-        log.info("Request to delete MenuGroup ID: {}", menuGroupId);
         menuGroupService.deleteMenuGroup(menuGroupId);
-        log.info("Successfully deleted MenuGroup ID: {}", menuGroupId);
 
         return ResponseEntity.ok().build();
     }
