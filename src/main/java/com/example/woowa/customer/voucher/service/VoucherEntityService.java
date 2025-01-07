f5d9fa1f0ea3e1100fb069f49c36f7a9c79ea524
JinjinBread <pge0720@naver.com>
2025-01-07 15:12:30 +0900

    refactor: Voucher 조회 로직 리팩토링 및 서비스 위임
    - 단일 책임 원칙(SRP) 강화

--- src/main/java/com/example/woowa/customer/voucher/service/VoucherEntityService.java
+++ src/main/java/com/example/woowa/customer/voucher/service/VoucherEntityService.java
@@ -5,6 +5,8 @@ import com.example.woowa.customer.voucher.repository.VoucherRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class VoucherEntityService {
@@ -12,6 +14,9 @@ public class VoucherEntityService {
     private final VoucherRepository voucherRepository;
 
     public Voucher findVoucherById(Long voucherId) {
+        if (Objects.isNull(voucherId)) {
+            return null;
+        }
         return voucherRepository.findById(voucherId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 voucherId 입니다."));
     }
