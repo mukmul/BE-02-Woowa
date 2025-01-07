d6a6fd8dfc86680fed83938c9da336953fcce38d
JinjinBread <pge0720@naver.com>
2025-01-07 15:30:07 +0900

    fix: AreaCode test code MockBean to MockitoBean and Remove unnecessary code

--- src/test/java/com/example/woowa/delivery/service/AreaCodeServiceTest.java
+++ src/test/java/com/example/woowa/delivery/service/AreaCodeServiceTest.java
@@ -10,12 +10,9 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 class AreaCodeServiceTest {
 
     @InjectMocks
