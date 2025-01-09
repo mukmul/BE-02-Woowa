ba9ff4d6c200106e30451056880319e1d2c74283
jinhosoon <jinho7329@gmail.com>
2025-01-09 10:45:32 +0900

    refactor - Add overloaded constructors to NotFoundException class

--- src/main/java/com/example/woowa/common/exception/NotFoundException.java
+++ src/main/java/com/example/woowa/common/exception/NotFoundException.java
@@ -2,7 +2,36 @@ package com.example.woowa.common.exception;
 
 public class NotFoundException extends RuntimeException {
 
+    private final String errorCode;
+    private final String resourceName;
+
+    private final Object resourceId;
+
     public NotFoundException(String message) {
         super(message);
+        this.errorCode = null;
+        this.resourceName = null;
+        this.resourceId = null;
+    }
+
+    public NotFoundException(ErrorMessage errorMessage) {
+        super(errorMessage.getMessage());
+        this.errorCode = errorMessage.name();
+        this.resourceName = null;
+        this.resourceId = null;
+    }
+
+    public NotFoundException(String resourceName, Object resourceId) {
+        super(String.format("Resource '%s' with id '%s' was not found.", resourceName, resourceId));
+        this.errorCode = "NOT_FOUND";
+        this.resourceName = resourceName;
+        this.resourceId = resourceId;
+    }
+
+    public NotFoundException(String errorCode, String resourceName, Object resourceId) {
+        super(String.format("[%s] - Resource '%s' with id '%s' was not found.", errorCode, resourceName, resourceId));
+        this.errorCode = errorCode;
+        this.resourceName = resourceName;
+        this.resourceId = resourceId;
     }
 }
